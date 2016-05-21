5f6966ac73a90b37e2596222514049451e844ecd
Tyler Dence <tyzoid.d@gmail.com>
2016-05-20 22:23:37 -0400

    Moved bunch of cron jobs into one script

---
+++ fetch_comics.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget "http://www.example.com/fetch/dilbert" >/dev/null 2>&1
+wget "http://www.example.com/fetch/pennyarcade" >/dev/null 2>&1
+wget "http://www.example.com/fetch/wtduck" >/dev/null 2>&1
+wget "http://www.example.com/fetch/doonesbury" >/dev/null 2>&1
+wget "http://www.example.com/fetch/pvp" >/dev/null 2>&1
+wget "http://www.example.com/fetch/tabletitans" >/dev/null 2>&1
