d05fc592a5d1bace1164646af859feefc1620bcb
Tyler Dence <tyzoid.d@gmail.com>
2016-05-20 21:56:13 -0400

    Make memcache optional

--- classes/CacheService.php
+++ classes/CacheService.php
@@ -4,34 +4,64 @@ class CacheService {
 	private $prefix;
 	private $mem;
 
-	public function __construct() {
+	public function __construct()
+	{
 		$this->prefix = "comicfeeds_";
-		$this->mem = new Memcached;
+
+		if (class_exists("Memcached"))
+			$this->mem = new Memcached;
+		else
+			$this->mem = null;
 	}
 
-	public function get($name) {
-		try {
+	public function get($name)
+	{
+		if (empty($this->mem))
+			return false;
+
+		try
+		{
 			return $this->mem->get($this->prefix . $name);
 		}
-		catch (Exception $ex) {
-			return FALSE;
+		catch (Exception $ex)
+		{
+			return false;
 		}
+
+		return true;
 	}
 
-	public function set($name, $value, $expirationSeconds) {
-		try {
+	public function set($name, $value, $expirationSeconds)
+	{
+		if (empty($this->mem))
+			return false;
+
+		try
+		{
 			$this->mem->set($this->prefix . $name, $value, time() + $expirationSeconds);
 		}
-		catch (Exception $ex) {
+		catch (Exception $ex)
+		{
+			return false;
 		}
+
+		return true;
 	}
 
-	public function getAllKeys() {
-		try {
+	public function getAllKeys()
+	{
+		if (empty($this->mem))
+			return false;
+
+		try
+		{
 			return $this->mem->getAllKeys();
 		}
-		catch (Exception $ex) {
-			return FALSE;
+		catch (Exception $ex)
+		{
+			return false;
 		}
+
+		return true;
 	}
 }
